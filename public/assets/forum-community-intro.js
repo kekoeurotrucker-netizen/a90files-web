87e51dd726ca04cb79cb33ea4a1458f9538022d2
kekoeurotrucker-netizen <kekoeurotrucker@gmail.com>
2026-09-15 23:32:07 +0200

    feat: present forum as a community project hub

---
+++ public/assets/forum-community-intro.js
@@ -0,0 +1,5 @@
+(()=>{
+'use strict';
+const intro=document.querySelector('.forum-intro p');
+if(intro)intro.textContent='Un espacio para compartir proyectos, pedir ayuda, dar feedback y hablar de software, juegos, música, contenido y cualquier otra creación de la comunidad. Aquí puedes enseñar lo que estás haciendo, descubrir proyectos de otros usuarios y encontrar personas con las que colaborar.';
+})();
